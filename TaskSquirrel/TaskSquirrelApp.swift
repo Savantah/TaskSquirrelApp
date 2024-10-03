78eb258ce7a1a6bd491256facb3aa2791c22b416
Savantah <stherclair@gmail.com>
2024-10-03 16:29:21 -0400

    Initial Commit

---
+++ TaskSquirrel/TaskSquirrelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskSquirrelApp.swift
+//  TaskSquirrel
+//
+//  Created by Star Lina on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskSquirrelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskSquirrelTests/TaskSquirrelTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskSquirrelTests.swift
+//  TaskSquirrelTests
+//
+//  Created by Star Lina on 10/3/24.
+//
+
+import Testing
+@testable import TaskSquirrel
+
+struct TaskSquirrelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
